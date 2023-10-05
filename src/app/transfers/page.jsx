6136a128da768b94c750135960cd4bf65f3d5d23
khalil <salamikhalil02@gmail.com>
2023-10-05 18:02:14 +0100

    coming soon page

--- src/app/transfers/page.jsx
+++ src/app/transfers/page.jsx
@@ -1,7 +1,12 @@
+import ComingSoon from "@/components/ComingSoon";
 import React from "react";
 
 function page() {
-	return <div>page</div>;
+	return (
+		<div>
+			<ComingSoon />
+		</div>
+	);
 }
 
 export default page;
