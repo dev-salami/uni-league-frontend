e65617ea92ca263c55b640de3aebc00ae440b781
khalil <salamikhalil02@gmail.com>
2023-10-13 07:54:13 +0100

    deleted unnecessary lines

--- src/app/fixtures/page.jsx
+++ src/app/fixtures/page.jsx
@@ -1,4 +1,3 @@
-import CreateFixtures from "@/components/CreateFixtures";
 import Fixtures from "@/components/Fixtures";
 import axios from "axios";
 
@@ -7,10 +6,8 @@ async function Page() {
 	const Matchday = 4;
 
 	const seasonMatchdayUrl = `https://uni-league.onrender.com/api/v1/fixtures/get-season-matchday?season=${Season}&matchday=${Matchday}`;
-	const seasonUrl = `localhost:5000/api/v1/fixtures/get-season-matchday?season=${Season}`;
 
 	const { data } = await axios.get(seasonMatchdayUrl);
-	// console.log(data);
 
 	return (
 		<div className="container mx-auto px-4">
